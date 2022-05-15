4d464678e863a5b9b8e4926661817a323f6e6faa
chiragkhimani <khimanichirag@gmail.com>
2022-05-15 22:47:57 +0530

    From Chirag again

--- src/com/automation/git/GitPractice.java
+++ src/com/automation/git/GitPractice.java
@@ -3,5 +3,6 @@ package com.automation.git;
 public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("From Chirag PC");
+		System.out.println("From Chirag Again");
 	}
 }
