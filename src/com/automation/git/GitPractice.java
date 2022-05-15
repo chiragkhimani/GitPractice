a44732bd7dd1135468b4cd7b1ca8cf084debccd8
chiragkhimani <khimanichirag@gmail.com>
2022-05-16 00:35:55 +0530

    login feature done

--- src/com/automation/git/GitPractice.java
+++ src/com/automation/git/GitPractice.java
@@ -4,5 +4,6 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("Master");
 		System.out.println("Login Feature Started");
+		System.out.println("Login completed");
 	}
 }
